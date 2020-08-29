ae2d0580a9a00e68415c2c32cf586bf2e5130b1f
Tricia Gosney <tricia.gosney@gtech.com>
2020-08-28 20:40:55 -0400

    profile page update

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/profile/:name">
+          <Route path="/profile">
             <Profile />
           </Route>
         </Switch>
